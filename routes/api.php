111ce3e7ca0c74df27b6068873d2c47cc53a8a5c
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2021-04-04 13:42:15 +0200

    Create Auth Controller Register User & Get Token

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 // Public routes
 // Route::resource('products', ProductController::class);
+ Route::post('/register',[AuthController::class,'register']);
  Route::get('/products',[ProductController::class,'index']);
  Route::get('/products/{id}',[ProductController::class,'show']);
  Route::get('/products/search/{name}',[ProductController::class,'search'])
